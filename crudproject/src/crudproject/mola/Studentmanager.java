8dc1ca5a01f594913501de3f63573c66f8f27d22
ChanWoo Park <diqksk@naver>
2021-01-23 14:26:39 +0900

    2차수정 (Delete까지 완료)

--- crudproject/src/crudproject/mola/Studentmanager.java
+++ crudproject/src/crudproject/mola/Studentmanager.java
@@ -1,11 +1,8 @@
 package crudproject.mola;
 
-import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Vector;
-import java.util.jar.Attributes.Name;
 
-import com.sun.org.apache.xpath.internal.operations.Equals;
 
 public class Studentmanager {
 	static Vector<Student> v = new Vector<Student>();
@@ -50,11 +47,64 @@ public class Studentmanager {
 	}
 
 	static void modify() {
+		System.out.print("누구의 정보를 수정하시겠습니까?");
+		String name, newname, clss, newclss, score, rank = new String();
+		int newrank;
+		Double newscore;
+		name = scanner.next();
+		for (int i = 0; i < v.size(); i++) {
+			if (name.equals(v.get(i).name)) {
+				System.out.print("어떤 정보를 수정하시겠습니까? 번호를 입력해주세요\n(1.name,2.class,3.score,4.rank)");
+				int modi = scanner.nextInt();
+				if (modi == 1) {
+					System.out.println("새로 바꿀 이름을 입력해주세요");
+					newname = scanner.next();
+					v.get(i).name = newname;
+					System.out.println(newname + "으로 변경 완료하였습니다.");
+				}
+				if (modi == 2) {
+					System.out.println("새로운 학과를 입력해주세요");
+					newclss = scanner.next();
+					v.get(i).clss = newclss;
+					System.out.println(newclss + "으로 변경 완료하였습니다.");
+				}
+				if (modi == 3) {
+					System.out.println("새로운 성적을 입력해주세요");
+					newscore = scanner.nextDouble();
+					v.get(i).score = newscore;
+					System.out.println(newscore + "점으로 변경 완료하였습니다.");
+				}
+				if (modi == 4) {
+					System.out.println("새로 등수를 입력해주세요");
+					newrank = scanner.nextInt();
+					v.get(i).rank = newrank;
+					System.out.println(newrank + "등으로 변경 완료하였습니다.");
+				}
 
+			}
+		}
 	}
 
 	static void delete() {
-
+		String select = "";
+		String y = "y";
+		String n = "n";
+		System.out.print("누구의 정보를 삭제하시겠습니까?");
+		String name = new String();
+		name = scanner.next();
+		for (int i = 0; i < v.size(); i++) {
+			if (name.equals(v.get(i).name)) {
+				System.out.println(name + "의 정보를 삭제하겠습니다 (y/n)");
+				select = scanner.next();
+				if (select.equals(y)) {
+					v.remove(i);
+					System.out.println("정보가 삭제 되었습니다");
+				}
+				if(select.equals(n)){
+					System.out.println("취소되었습니다.");
+				}
+			}
+		}
 	}
 
 	static void end() {
@@ -76,7 +126,6 @@ public class Studentmanager {
 			scanner.next();
 			menu();
 		}
-		System.out.println();
 		return scan;
 
 	}
